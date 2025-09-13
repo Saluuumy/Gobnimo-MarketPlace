9847b2731633636f4d655e0ac927e25ec66de57d
Saluuumy <salmamacash@gmail.com>
2025-09-13 23:03:18 +0300

    hosting on render.com

--- vercel_build.py
+++
@@ -1,9 +0,0 @@
-# vercel_build.py
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Ecommerce.settings")
-application = get_wsgi_application()
-
-# Vercel requires this named "app"
-app = application
