9d6dd9b264817bb304c592bc4f764ca7edbe7402
Saluuumy <salmamacash@gmail.com>
2025-10-06 10:34:29 +0300

    hosting on render.com

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -110,7 +110,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-
+WHITENOISE_ROOT = STATIC_ROOT 
 
 #MEDIA_URL = '/media/'
 #MEDIA_ROOT = BASE_DIR / 'media'
