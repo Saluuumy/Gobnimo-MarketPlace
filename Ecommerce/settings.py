3d578a929aadcea54a927bf646930d502039ebd1
Saluuumy <salmamacash@gmail.com>
2026-06-22 22:53:03 +0300

    Fix missing script.js

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -18,7 +18,16 @@ environ.Env.read_env(str(BASE_DIR / ".env"))
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS",
+    default=[
+        "localhost",
+        "127.0.0.1",
+        "waaheen-d8bzabe3fehygpgg.westeurope-01.azurewebsites.net",
+        "169.254.129.2",  # Azure internal health check IP
+    ],
+)
+
 CSRF_TRUSTED_ORIGINS = env.list(
     "CSRF_TRUSTED_ORIGINS",
     default=[
